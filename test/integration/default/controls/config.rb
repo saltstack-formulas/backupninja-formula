c9de74b0ec468cc41dc4efe25e31e370bfbec592
Eric Veiras Galisson <eric@sietch-tabr.com>
2020-11-21 18:47:35 +0100

    test: implement inspec tests

---
+++ test/integration/default/controls/config.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+control 'backupninja configuration' do
+  title 'should match desired lines'
+
+  ['11-custom.sh', '12-custom-rsync.sh'].each do |file|
+    describe file("/etc/backup.d/#{file}") do
+      it { should be_file }
+      its('owner') { should eq 'root' }
+      its('group') { should eq 'root' }
+      its('mode') { should cmp '0640'}
+    end
+  end
+end
